1acccc48170c6f1a0e0f67628e430676722033d0
JeroenBos <jer123bos@gmail.com>
2019-03-12 14:37:27 +0100

    Create template

---
+++ template.ts
@@ -0,0 +1,24 @@
+import { OptionalToMissing, assert, IsExact } from './typeHelper';
+import { PrimitiveTypes, BaseTypeDescriptions, nullable, optional, optionalNullable, possiblyUndefined, possiblyNullOrUndefined, composeDescriptions } from './built-ins';
+import { createCreateFunction, TypeSystem, DebugTypeSystem } from '.';
+import { TypeDescriptionsFor } from './ITypeDescription';
+
+interface D {
+}
+
+//////////////////
+
+export type checkableTypes = OptionalToMissing<{
+    'D': D
+}>
+
+const create = <T extends object>() => createCreateFunction<checkableTypes & PrimitiveTypes, T>();
+
+export class AllTypeDescriptions extends BaseTypeDescriptions implements TypeDescriptionsFor<checkableTypes> {
+    public readonly 'D' = create<D>()({});
+}
+
+export default new TypeSystem(new AllTypeDescriptions());
+
+type erroneousTypes = DebugTypeSystem<checkableTypes>
+assert<IsExact<erroneousTypes, {}>>(true);
