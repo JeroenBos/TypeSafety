2d0aed12e7a17f79465a513e06fbbf60b22d9fb2
JeroenBos <jer123bos@gmail.com>
2019-04-11 16:24:35 +0200

    Test any/null/undefined

---
+++ tests/any-null-undefined.testsystem.ts
@@ -0,0 +1,41 @@
+
+import { createCreateFunction, TypeSystem, DebugTypeSystem, DescriptionKeys } from "../typesystem";
+import { PrimitiveTypes, possiblyUndefined, nullable, possiblyNullOrUndefined, optional, BaseTypeDescriptions, missingType } from "../built-ins";
+import { TypeDescriptionsFor } from "../ITypeDescription";
+import { OptionalToMissing, assert, IsExact, GetKey } from "../typeHelper";
+
+class A { }
+interface C {
+    a: string,
+    b: string | null,
+    c: string | undefined,
+    d: string | null | undefined,
+    e: A,
+    f: A | null,
+    g: A | undefined,
+    h: A | null | undefined,
+    i: null,
+    j: undefined,
+    k: null | undefined,
+    l: any
+}
+
+///////////////////////////
+
+export type CheckableTypes = OptionalToMissing<{
+    'C': C,
+}>
+type cDescription = DescriptionKeys<'C', CheckableTypes & PrimitiveTypes>;
+
+assert<IsExact<cDescription['a'], 'string'>>(true);
+assert<IsExact<cDescription['b'], 'nullable string'>>(true);
+assert<IsExact<cDescription['c'], 'string?'>>(true);
+assert<IsExact<cDescription['d'], 'nullable string?'>>(true);
+assert<IsExact<cDescription['e'], 'any!'>>(true);
+assert<IsExact<cDescription['f'], '!undefined'>>(true);
+assert<IsExact<cDescription['g'], '!null'>>(true);
+assert<IsExact<cDescription['h'], 'any'>>(true);
+assert<IsExact<cDescription['i'], 'null'>>(true);
+assert<IsExact<cDescription['j'], 'undefined'>>(true);
+assert<IsExact<cDescription['k'], 'any'>>(true);
+assert<IsExact<cDescription['l'], 'any' | '!null' | '!undefined' | 'any!'>>(true);
